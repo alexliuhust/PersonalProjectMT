d9e48598afdf1419ce22444dd17aa979a97b0902
Zehua(Alexander) Liu <68038532+alexliuhust@users.noreply.github.com>
2020-11-04 20:03:08 -0500

    Configured H2; created ProjectRepository

---
+++ PPMTproject/src/main/java/io/alexspringboot/ppmtool/repository/ProjectRepository.java
@@ -0,0 +1,11 @@
+package io.alexspringboot.ppmtool.repository;
+
+import io.alexspringboot.ppmtool.domain.Project;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProjectRepository extends CrudRepository<Project, Long> {
+    @Override
+    Iterable<Project> findAllById(Iterable<Long> iterable);
+}
