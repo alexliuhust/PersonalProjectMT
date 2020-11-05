04705c24b48b2a39e2e01ff3dc46d7a0b643c5c2
Zehua(Alexander) Liu <68038532+alexliuhust@users.noreply.github.com>
2020-11-04 23:32:34 -0500

    Custom Exceptions for Unique Project Identifiers

--- PPMTproject/src/main/java/io/alexspringboot/ppmtool/services/ProjectService.java
+++ PPMTproject/src/main/java/io/alexspringboot/ppmtool/services/ProjectService.java
@@ -1,6 +1,7 @@
 package io.alexspringboot.ppmtool.services;
 
 import io.alexspringboot.ppmtool.domain.Project;
+import io.alexspringboot.ppmtool.exceptions.ProjectIdException;
 import io.alexspringboot.ppmtool.repository.ProjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,7 +13,14 @@ public class ProjectService {
     private ProjectRepository projectRepository;
 
     public Project saveOrUpdateProject(Project project) {
+        try {
+            project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+            return projectRepository.save(project);
+        } catch (Exception e) {
+            throw new ProjectIdException(
+                    "Project ID '" + project.getProjectIdentifier().toUpperCase() + "' already exists");
+        }
 
-        return projectRepository.save(project);
+
     }
 }
