e1e8edf778f6a69aeec97dea50b93c0b3112421d
Zehua(Alexander) Liu <68038532+alexliuhust@users.noreply.github.com>
2020-11-06 17:04:10 -0500

    Add the firsr component

--- ppmtool-react-client/src/App.js
+++ ppmtool-react-client/src/App.js
@@ -1,13 +1,11 @@
-import logo from './logo.svg';
+import React, { Component } from 'react'
 import './App.css';
+import Dashboard from './components/Dashboard';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Welcom to PPMTOOL</h1>
-      </header>
+      <Dashboard />
     </div>
   );
 }
