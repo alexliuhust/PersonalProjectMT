9919ffcf88bd67935e2122c8bb4f420fffa915a3
Zehua(Alexander) Liu <68038532+alexliuhust@users.noreply.github.com>
2020-11-06 20:01:23 -0500

    CreateProjectButton; AddProject Route

--- ppmtool-react-client/src/components/Dashboard.js
+++ ppmtool-react-client/src/components/Dashboard.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import CreateProjectButton from './Project/CreateProjectButton';
 import ProjectItem from './Project/ProjectItem';
 
 class Dashboard extends Component {
@@ -10,9 +11,7 @@ class Dashboard extends Component {
                         <div className="col-md-12">
                             <h1 className="display-4 text-center">Projects</h1>
                             <br />
-                            <a href="ProjectForm.html" className="btn btn-lg btn-info">
-                                Create a Project
-                            </a>
+                            <CreateProjectButton />
                             <br />
                             <hr />
 
