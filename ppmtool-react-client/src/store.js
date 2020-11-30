b9ec08205b4dd5d7233130d7248f6edd4c95ac0d
Zehua(Alexander) Liu <68038532+alexliuhust@users.noreply.github.com>
2020-11-30 12:38:40 -0500

    Get validation errors from Redux

--- ppmtool-react-client/src/store.js
+++ ppmtool-react-client/src/store.js
@@ -1,27 +1,28 @@
-import {createStore, applyMiddleware, compose} from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
 import rootReducer from "./reducer";
 
-const initialState = {};
+const initalState = {};
 const middleware = [thunk];
 
 let store;
 
-
 if (window.navigator.userAgent.includes("Chrome")) {
-    store = createStore(
-        rootReducer, 
-        initialState, 
-        compose(applyMiddleware(...middleware), 
-            window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
-    );
+  store = createStore(
+    rootReducer,
+    initalState,
+    compose(
+      applyMiddleware(...middleware),
+      window.__REDUX_DEVTOOLS_EXTENSION__ &&
+        window.__REDUX_DEVTOOLS_EXTENSION__()
+    )
+  );
 } else {
-    store = createStore(
-        rootReducer, 
-        initialState, 
-        compose(applyMiddleware(...middleware))
-    );
+  store = createStore(
+    rootReducer,
+    initalState,
+    compose(applyMiddleware(...middleware))
+  );
 }
-
 
 export default store;
