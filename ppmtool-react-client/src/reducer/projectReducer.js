6b21cce36a30cded08f4d9f084653ff118243ff5
Zehua(Alexander) Liu <68038532+alexliuhust@users.noreply.github.com>
2020-12-21 17:47:08 -0500

    Delete Project: 01 have to refresh the page

--- ppmtool-react-client/src/reducer/projectReducer.js
+++ ppmtool-react-client/src/reducer/projectReducer.js
@@ -1,4 +1,4 @@
-import { GET_PROJECT, GET_PROJECTS } from "../actions/types";
+import { DELETE_PROJECT, GET_PROJECT, GET_PROJECTS } from "../actions/types";
 
 const initialState = {
   projects: [],
@@ -17,6 +17,12 @@ export default function (state = initialState, action) {
       return {
         ...state,
         project: action.payload,
+      };
+
+    case DELETE_PROJECT:
+      return {
+        ...state,
+        // project: action.payload,
       };
 
     default:
