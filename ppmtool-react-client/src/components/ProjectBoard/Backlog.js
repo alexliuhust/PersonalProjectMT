31097f8be8be45f3a2dd701ad7165fd05101596f
Zehua(Alexander) Liu <68038532+alexliuhust@users.noreply.github.com>
2021-01-14 17:25:59 -0500

    Outlook changes - Task Lists Card Header

--- ppmtool-react-client/src/components/ProjectBoard/Backlog.js
+++ ppmtool-react-client/src/components/ProjectBoard/Backlog.js
@@ -38,7 +38,7 @@ class Backlog extends Component {
         <div className="row">
           <div className="col-md-4">
             <div className="card text-center mb-2">
-              <div className="card-header bg-secondary text-white">
+              <div className="card-header text-dark" id="todocardheader">
                 <h3>TO DO</h3>
               </div>
             </div>
@@ -47,16 +47,16 @@ class Backlog extends Component {
 
           <div className="col-md-4">
             <div className="card text-center mb-2">
-              <div className="card-header bg-primary text-white">
-                <h3>In Progress</h3>
+              <div className="card-header text-dark" id="inprogresscardheader">
+                <h3>IN PROGRESS</h3>
               </div>
             </div>
             {inprogressTasks}
           </div>
           <div className="col-md-4">
             <div className="card text-center mb-2">
-              <div className="card-header bg-success text-white">
-                <h3>Done</h3>
+              <div className="card-header text-dark" id="donecardheader">
+                <h3>DONE</h3>
               </div>
             </div>
             {doneTasks}
